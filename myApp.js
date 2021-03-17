796b13657263e7610c4f44f022bca83c3941278b
Joshie-Finster <77668881+Joshie-Finster@users.noreply.github.com>
2021-03-17 09:34:59 +0000

    deleted first app.get()

--- myApp.js
+++ myApp.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var app = express(console.log("Hello World"));
-app.get( '/', (req,res) =>{
-    //res.send("Hello Express");
-    res.sendFile( __dirname + "/views/index.html");
-})
+
 app.use("/public", express.static(__dirname + "/public"));
 app.get('/json', (req,res)=>{
   if(process.env.MESSAGE_STYLE ==="uppercase"){
