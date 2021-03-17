90b6908dad96521bff558cfc95d673d9bba49064
Joshie-Finster <77668881+Joshie-Finster@users.noreply.github.com>
2021-03-17 09:38:35 +0000

    res

--- myApp.js
+++ myApp.js
@@ -7,9 +7,9 @@ app.get( '/', (req,res) =>{
 app.use("/public", express.static(__dirname + "/public"));
 app.get('/json', (req,res)=>{
   if (process.env.VAR_NAME === "allCaps") {
-    response = "Hello json".toUpperCase();
+    res = "Hello json".toUpperCase();
   } else {
-    response = "Hello json";
+    res = "Hello json";
   }
 });
 
