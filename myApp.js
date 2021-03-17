b72af042e8efdc036914311695a95f9e635b509c
Joshie-Finster <77668881+Joshie-Finster@users.noreply.github.com>
2021-03-17 09:42:51 +0000

    res.json

--- myApp.js
+++ myApp.js
@@ -5,11 +5,12 @@ app.get( '/', (req,res) =>{
     res.sendFile( __dirname + "/views/index.html");
 })
 app.use("/public", express.static(__dirname + "/public"));
+
 app.get('/json', (req,res)=>{
-  if (process.env.VAR_NAME === "uppercase") {
-    res = "Hello json".toUpperCase();
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({"message":"HELLO JSON"});
   } else {
-    res = "Hello json";
+    res.json({"message":"Hello json"});
   }
 });
 
