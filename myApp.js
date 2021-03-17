15965df277a7e808d626f3a37a77840181632b8c
Joshie-Finster <77668881+Joshie-Finster@users.noreply.github.com>
2021-03-17 10:03:38 +0000

    wtf

--- myApp.js
+++ myApp.js
@@ -1,50 +1,18 @@
-var express = require('express');
+var express = require("express");
 var app = express(console.log("Hello World"));
-app.get( '/', (req,res) =>{
-    //res.send("Hello Express");
-    res.sendFile( __dirname + "/views/index.html");
-})
-
-app.get('/json', (req,res)=>{
-  if(process.env.MESSAGE_STYLE==="uppercase"){
-  res.json({message:"HELLO JSON"})
-  }else{
-  res.json({message:"Hello json"})
-}
+app.get("/", (req, res) => {
+  //res.send("Hello Express");
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("/json", (req, res) => {
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({ message: "HELLO JSON" });
+  } else {
+    res.json({ message: "Hello json" });
+  }
+});
 
 app.use("/public", express.static(__dirname + "/public"));
 
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
